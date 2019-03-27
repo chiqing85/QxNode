04040116e0557478e593bfd340851dd8ba9d7fcb
chiqing_85 <chiqing1985@gmail.com>
2019-03-27 09:32:29 +0800

    env

--- config/app.js
+++ config/app.js
@@ -1,10 +1,10 @@
 // 配置
-
-// 启动配置
-module.exports.options = {
+module.exports.env = {
+	// 启动配置
 	host: 'localhost',
-	port: 8080
+	port: 8080,
+	// 调试模式
+	app_debug: true,
+	// 路由后缀
+	url_html_suffix: '.html'
 }
-
-// 路由后缀
-module.exports.url_html_suffix = '.html'
--- web/index.js
+++ web/index.js
@@ -1,8 +1,8 @@
 // 全局 程序根目录
 global.web_path = process.cwd()
 
-let	c = require(web_path + '/config/app'),
+let	env = require(web_path + '/config/app'),
 	index = require(web_path + '/lib/index')
 
-let app = new index( c )
+let app = new index( env )
 app.init()
