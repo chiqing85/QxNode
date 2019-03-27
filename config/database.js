9c7685e964bfd9f5382891c4ec66a487d0682b77
chiqing_85 <chiqing1985@gmail.com>
2019-03-27 09:49:10 +0800

    database

---
+++ config/database.js
@@ -0,0 +1,16 @@
+// 数据库配置文件
+// sql 配置
+module.exports.database = {
+	// 数据库类型
+	type: 'mysql',
+	//服务器地址
+	host: '127.0.0.1',
+	// 数据库名
+	database: 'homestead',
+	// 用户名
+	username: 'homestead',
+	// 密码
+	password: 'secret',
+	// 服务器地址
+	port: 3306
+}
